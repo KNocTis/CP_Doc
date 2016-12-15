7f093ffc2a0278e3be1605522a40eb74b7677c18
KNocTis <kopczc@gmail.com>
2016-12-16 03:53:23 +0800

    Add scale factor

--- wordpressCode.js
+++ wordpressCode.js
@@ -1,32 +1,41 @@
 var testMode = false;
 var transparentImageUrl = testMode? "http://172.106.32.119/wp-content/uploads/2016/12/EEFA85-0.5.png" : "http://172.106.32.119/wp-content/uploads/2016/12/NFFFFFF-0.png"
 
+var maxWidth = 620; //depends on the max width of Wordpress post page
+
 function htmlCodeForWPwithBlocks(blocks, image)
 {
+    //Protection
     if(blocks == undefined || blocks.length == 0){return};
     
+    //
+    var scaleFactor = 1;
+    if (maxWidth < image.width){
+        scaleFactor = maxWidth / image.width ;
+    }
+        
     let backgroundStr = "url(\'" + image.src + "\')"
         
     var aDiv = document.createElement('div');
     aDiv.className = "size-full";
-    aDiv.style.width = image.width + "px";
-    aDiv.style.height = image.height + "px";
+    aDiv.style.width = image.width * scaleFactor + "px";
+    aDiv.style.height = image.height * scaleFactor + "px";
     aDiv.style.backgroundImage = backgroundStr;
+    aDiv.style.backgroundSize = image.width * scaleFactor + "px" + " " + image.height * scaleFactor + "px";
 
-    
     for (let i = 0; i < blocks.length; i++)
     {
         let aA = document.createElement('a');
         aA.href = blocks[i].url;
         aA.target = "_blank";
         aA.style.position = "absolute";
-        aA.style.marginLeft = blocks[i].left + "px";
-        aA.style.marginTop = blocks[i].top + "px";
+        aA.style.marginLeft = blocks[i].left * scaleFactor + "px";
+        aA.style.marginTop = blocks[i].top * scaleFactor + "px";
         
         let aImg = document.createElement('img');
         aImg.src = transparentImageUrl;
-        aImg.style.width = blocks[i].getWidth() + "px";
-        aImg.style.height = blocks[i].getHeight() + "px";
+        aImg.style.width = blocks[i].getWidth() * scaleFactor + "px";
+        aImg.style.height = blocks[i].getHeight() * scaleFactor + "px";
         
         aA.appendChild(aImg);
         
